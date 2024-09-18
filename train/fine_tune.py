7c3f38ed0040b29db2c434587284ce3c82ce6ff3
nasko25 <apashov0@gmail.com>
2024-09-18 22:33:39 +0300

    feat(train): add more data to the dataset

--- train/fine_tune.py
+++ train/fine_tune.py
@@ -8,10 +8,19 @@ import sys
 import json
 import os
 
+class DatasetEntry:
+    def __init__(self, url, name, artist):
+        self.url = url
+        self.name = name
+        self.artist = artist
+    
+    def get_file_name(self):
+        return "_".join(self.name.split(" ")).lower()
+
 # generate a json metadata file for each audio entry in the dataset
-def generate_json_metadata(dataset_name, artist):
+def generate_json_metadata(dataset_name, file_name, artist):
     songs = {}
-    with open(f"./egs/tmp/data_{"_".join(dataset_name.split(" ")).lower()}.jsonl") as songs_data:
+    with open(f"./egs/tmp/data_{file_name}.jsonl") as songs_data:
         #print(songs_data.read())
         songs_json = list(filter(lambda s: s != "", songs_data.read().split("\n")))
     #     print(songs_json)
@@ -28,11 +37,17 @@ def generate_json_metadata(dataset_name, artist):
             json_file.write(json.dumps(file_info))
 
 # download audio files to be used in the dataset
-url = "https://drive.google.com/uc?id=1pFgX8UtrjTmtdCGJkxmhrQJTXt2H8xOD"
-gdown.download(url, "imuno.zip")
+data_entries = [
+    DatasetEntry("https://drive.google.com/uc?id=1pFgX8UtrjTmtdCGJkxmhrQJTXt2H8xOD", "Beat Hype", "Imuno"),
+    DatasetEntry("https://drive.google.com/uc?id=1mp9vTPHoMY-pcAtbmT7WfbnhkDbssn0g", "Dark Forces", "J1K"),
+    DatasetEntry("https://drive.google.com/uc?id=1AaYk89p8GJOkXNUE6VBfe1fLOMMRDABh", "Wolves and Sheep", "Marswell"),
+    DatasetEntry("https://drive.google.com/uc?id=1ktT3zej6yOHMBmnPKvMIRHXlvwmk0_TU", "007", "Mobb"),
+    DatasetEntry("https://drive.google.com/uc?id=1EWe7aCHk41yEeiYHETvnwRBqh2n4AZOx", "0201", "Mobb"),
+    DatasetEntry("https://drive.google.com/uc?id=1nNYvGgxDukU1vlCiSdUzaotYqsSUgoNn", "Instros", "TKMAK"),
+    DatasetEntry("https://drive.google.com/uc?id=1Gq-ikxkWz9ap4Khau97WjWstvo82eFIY", "Lost Instrumentals Vol 1", "WORST"),
 
-with ZipFile("./imuno.zip", 'r') as zip_file:
-    zip_file.extractall("./data/")
+    # DatasetEntry("TODO", "Various", "Var"),
+]
 
 try:
     subprocess.check_call(["ffmpeg", "-version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -41,9 +56,10 @@ except:
     print("     sudo apt-get install ffmpeg")
     sys.exit(-1)
 
-shutil.move("data/", "audiocraft/data/")
+shutil.copyfile("config/musicgen_base_32khz_custom.yaml", "audiocraft/config/solver/musicgen/musicgen_base_32khz_custom.yaml")
 
 os.chdir("audiocraft/")
+os.mkdir("data/")
 
 with open("config/dset/audio/custom.yaml", 'w') as f:
     f.write("""# @package __global__
@@ -62,16 +78,24 @@ subprocess.check_call([sys.executable, "-m", "pip", "install", "torch==2.1.0"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools wheel"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", "."])
 
-subprocess.check_call([sys.executable, "-m", "audiocraft.data.audio_dataset", "data/Beat Hype", "egs/tmp/data_beat_hype.jsonl"])
+os.remove("egs/custom/data.jsonl")
 
-generate_json_metadata("Beat Hype", "Imuno")
-# TODO: add more data sources
+for entry in data_entries:
+    gdown.download(entry.name, f"{entry.get_file_name()}.zip")
 
-# concatenate all existing dataset information in a single data.jsonl file that will be used by dora
-with open("egs/custom/data.jsonl", "w") as data, open("egs/tmp/data_beat_hype.jsonl", "w") as ds1:
-    data.write(ds1.read())
+    with ZipFile(f"./{entry.get_file_name()}.zip", 'r') as zip_file:
+        zip_file.extractall(f"./data/{entry.name}")
+    os.remove(f"./{entry.get_file_name()}.zip")
 
-shutil.move("data/Beat Hype", "data/")
+    subprocess.check_call([sys.executable, "-m", "audiocraft.data.audio_dataset", f"data/{entry.name}", f"egs/tmp/data_{entry.get_file_name()}.jsonl"])
+
+    generate_json_metadata(entry.name, entry.get_file_name(), entry.artist)
+
+    # concatenate all existing dataset information in a single data.jsonl file that will be used by dora
+    with open("egs/custom/data.jsonl", "a") as data, open(f"egs/tmp/data_{entry.get_file_name()}.jsonl", "w") as ds:
+        data.write(ds.read())
+
+    shutil.move(f"data/{entry.name}", "data/")
 
 env = os.environ.copy()
 # env["USER"] = "<user name>" # I needed to add this when testing in colab and kaggle
@@ -81,12 +105,3 @@ subprocess.check_call(["dora", "run", "-d", "solver=musicgen/musicgen_base_32khz
 
 # NOTE: You can use this command to restart training from a checkpoint:
 #  USER=root HYDRA_FULL_ERROR=1 dora run -d solver=musicgen/musicgen_base_32khz_custom.yaml model/lm/model_scale=small continue_from=/tmp/audiocraft_root/xps/29c15616/checkpoint.th conditioner=text2music dset=audio/custom
-
-sys.exit(0)
-
-# export the model
-# first, in order to get the model signature, run:
-#  dora info solver=musicgen/musicgen_base_32khz_custom.yaml model/lm/model_scale=small continue_from=//pretrained/facebook/musicgen-small conditioner=text2music dset=audio/custom
-#  (based on the command you used for fine-tuning)
-
-# TODO
