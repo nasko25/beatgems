2f5813c492af2eeef81df3587323c51bd7e9269e
nasko25 <apashov0@gmail.com>
2024-09-20 17:14:05 +0300

    feat(train): add model signature as a cli argument to export_model

--- train/export_model.py
+++ train/export_model.py
@@ -7,9 +7,27 @@
 
 from audiocraft.utils import export
 from audiocraft import train
+import argparse
 
-SIG = "ADD MODEL SIGNATURE HERE"
+SIG = "" # ADD MODEL SIGNATURE HERE
 
+parser = argparse.ArgumentParser(
+    prog='export_model',
+    description='audiocraft model exporting script',
+    epilog='Export any fine-tuned audiocraft model after training with fine_tune.py')
+
+parser.add_argument('-s', '--signature')
+args = parser.parse_args()
+
+if SIG != "" and args.signature is not None:
+    raise Exception("You have set a signature both in the script and as a command argument. Make sure this is not a mistake.")
+elif SIG == "":
+    SIG = args.signature
+
+if SIG is None:
+    raise Exception("Model signature was not set. Check the README on how to set it")
+
+print(f"\nUsing model signature {SIG}")
 xp = train.main.get_xp_from_sig(SIG)
 export.export_lm(xp.folder / 'checkpoint.th', '/checkpoints/my_audio_lm/state_dict.bin')
 
