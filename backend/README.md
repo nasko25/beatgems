4ffbebc7f421dc4b31fbc1e1829bd4443d8df492
nasko25 <apashov0@gmail.com>
2024-09-27 19:10:01 +0300

    feat(backend): add go backend

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# beatgems backend
+
+a simple backend written in Go
+
+Run with:
+
+```bash
+go build main.go && ./main
+```
---
+++ backend/main.go
@@ -0,0 +1,18 @@
+//go:build ignore
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "api handling /%s", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
