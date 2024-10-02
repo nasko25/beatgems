22b6526e21939f80435ed15449d88809349b980b
nasko25 <apashov0@gmail.com>
2024-10-02 17:46:53 +0300

    fix(train): fix pip install and add a package version for setuptools

--- train/fine_tune.py
+++ train/fine_tune.py
@@ -96,7 +96,7 @@ datasource:
 """)
 
 subprocess.check_call([sys.executable, "-m", "pip", "install", "torch==2.1.0"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools wheel"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools==65.0.2", "wheel"])
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-e", "."])
 
 try:
